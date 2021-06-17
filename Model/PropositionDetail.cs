708f29a019f165ad791fd6a46c59883b6a3988e4
Valentin Venom <valentin.venom@gmail.com>
2021-06-17 13:39:23 +0200

    Module recherche + JS

---
+++ Model/PropositionDetail.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fr.EQL.AI109.Tontapat.Model
+{
+    class PropositionDetail : Proposition
+    {
+    }
+}
